5af7576e0d7250daadae8fa75adf1e1671be4a3f
JUAN-BH <juanmita_bh@hotmail.com>
2023-05-26 11:36:43 -0500

    feat: :sparkles: Cats in session
    Random cats will remain even when the user go to another page

--- src/hooks/useCats.tsx
+++ src/hooks/useCats.tsx
@@ -3,14 +3,27 @@ import { CatImg } from "../ts/models/cat.model";
 import { getImgs } from "../services/getImages";
 
 export const useCats = (numCats: number) => {
-  const [cats, setCats] = useState<CatImg[]>([]);
+  let catsInSession: CatImg[];
+
+  const catsSessionActive: string | null =
+    sessionStorage.getItem("catsInSession");
+  if (catsSessionActive) {
+    catsInSession = JSON.parse(catsSessionActive);
+  } else {
+    sessionStorage.setItem("catsInSession", "[]");
+    catsInSession = [];
+  }
+
+  const [cats, setCats] = useState<CatImg[]>(catsInSession);
 
   const getCats = async () => {
     const rtaCats = await getImgs(numCats);
+    sessionStorage.setItem("catsInSession", JSON.stringify(rtaCats));
     setCats((prev: CatImg[]) => [...prev, ...rtaCats]);
   };
 
   const clearCats = () => {
+    sessionStorage.setItem("catsInSession", "[]");
     setCats([]);
   };
 

8a52eadd9a2010f333674a2544450e83b171068f
JUAN-BH <juanmita_bh@hotmail.com>
2023-06-02 15:40:04 -0500

    fix: :bug: Fixing axios instance

--- src/services/apiSettings.ts
+++ src/services/apiSettings.ts
@@ -4,7 +4,7 @@ const APIKEY = `${import.meta.env.VITE_API_KEY}`;
 const APIURL = "https://api.thecatapi.com/v1/";
 export const API = axios.create({
   baseURL: APIURL,
-  params: {
-    api_key: APIKEY,
+  headers: {
+    "X-API-KEY": APIKEY,
   },
 });
